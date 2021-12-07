d669e49f3626c2b4b2771db7de640df5ea5b4057
amy-chinlai <amy@actionnetwork.org>
2021-12-07 13:55:58 -0500

    commit tree 15d7a890ce7e8d1542c776098086fa9ae20d967d

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const time_split = time.split(':')
+  const hour = parseInt(time_split[0])
+  if (hour < 12) {
+    return `Good Morning`;
+  }
+  if (hour < 17) {
+    return `Good Afternoon`;
+  }
+  if (hour < 24) {
+    return `Good Evening`;
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(aString) {
+  document.getElementById("greeting").innerHTML = aString
+}
